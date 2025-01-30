65fb6afeeaa082efb293bd596ed507faf97610f8
Curry5004 <yunjin5004@naver.com>
2025-01-30 09:14:07 +0900

    Initial commit

---
+++ src/main/java/hello/kyh_springbasic_mvc_part_1_practice/KyhSpringBasicMvcPart1PracticeApplication.java
@@ -0,0 +1,13 @@
+package hello.kyh_springbasic_mvc_part_1_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringBasicMvcPart1PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyhSpringBasicMvcPart1PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/kyh_springbasic_mvc_part_1_practice/KyhSpringBasicMvcPart1PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.kyh_springbasic_mvc_part_1_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhSpringBasicMvcPart1PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
